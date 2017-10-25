08536a98ef04e2e163155448d7047be7169baeaf
Andrei Nishanov <mazahaker93@gmail.com>
2017-10-25 18:23:21 +0600

    Customized routes for locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'products#index'
-
-  devise_for :users
-
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do 
+	  root 'products#index'
+	  devise_for :users
+	end
   namespace :admin do
     root 'products#index'
     resources :products
     resources :categories
   end
+  match "*path", to: redirect("/#{I18n.default_locale}/%{path}"), via: :all
+  match "", to: redirect("/#{I18n.default_locale}/"), via: :all
 end
