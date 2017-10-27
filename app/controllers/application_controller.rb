754b445816c6f5ffb7f766e3cd9652eedb31373c
Andrei Nishanov <mazahaker93@gmail.com>
2017-10-27 11:38:05 +0600

    Add avatar for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name 
-    devise_parameter_sanitizer.for(:sign_up) << :locale
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :avatar
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :avatar
   end
 
   def admin_user
