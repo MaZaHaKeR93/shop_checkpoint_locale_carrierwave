ae2d0f7282ed72793e94a521a9ebeafb05d8f916
Andrei Nishanov <mazahaker93@gmail.com>
2017-10-27 13:21:35 +0600

    edit seeds - add upload image for product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,7 +7,6 @@ class Admin::ProductsController < AdminController
     @product = Product.new(product_params)
 
     if @product.save
-      # save_picture
       redirect_to [:admin, @product]
     else
       render 'new'
@@ -22,7 +21,6 @@ class Admin::ProductsController < AdminController
     @product = Product.find(params[:id])
 
     if @product.update(product_params)
-      # save_picture
       redirect_to [:admin, @product]
     else
       render 'edit'
@@ -47,16 +45,4 @@ class Admin::ProductsController < AdminController
   def product_params
     params.require(:product).permit(:title_en, :title_ru, :description_en, :description_ru, :price, :category_id, :image)
   end
-
-  # def save_picture
-  #   uploaded_file = params[:product][:picture]
-
-  #   unless uploaded_file.nil?
-  #     new_file_path = Rails.root.join('public', 'uploads', @product.id.to_s)
-
-  #     File.open(new_file_path, 'wb') do |file|
-  #       file.write uploaded_file.read
-  #     end
-  #   end
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# def copy_image_fixture(file_name, id)
-#   dir = Rails.root.join('public', 'uploads')
-#   Dir.mkdir(dir) unless File.directory?(dir)
-#   FileUtils.cp(file_name, dir.join(id.to_s))
-# end
-
 cpus_category = Category.create(name_en: 'CPUs', name_ru: "Процессоры")
 motherboards_category = Category.create(name_en: 'Motherboards', name_ru: "Мат. платы")
 ram_category = Category.create(name_en: 'RAM', name_ru: "Оперативная память")
@@ -11,15 +5,12 @@ hdd_category = Category.create(name_en: 'HDDs', name_ru: "Жесткие дис�
 
 fixtures_category = Category.create(name_en: 'Fixtures', name_ru: "Фикстуры" )
 
-# fixtures_path = Rails.root.join('app', 'assets', 'images', 'fixtures')
-
 cpu = Product.create(title_en: 'Intel Core i7',
 										title_ru: 'Процессор Intel Core i7',
 										price: 300, 
 										description_en: Faker::Lorem.paragraph,
 										description_ru: Faker::Lorem.paragraph,
 										category: cpus_category)
-# copy_image_fixture(fixtures_path.join('cpu.jpg'), cpu.id)
 
 ram = Product.create(title_en: '4GB DDR3 RAM',
 										title_ru: '4GB DDR3 ОЗУ',
@@ -27,7 +18,6 @@ ram = Product.create(title_en: '4GB DDR3 RAM',
 										description_en: Faker::Lorem.paragraph, 
 										description_ru: Faker::Lorem.paragraph,
 										category: ram_category)
-# copy_image_fixture(fixtures_path.join('ram.jpg'), ram.id)
 
 hdd = Product.create(title_en: '1TB Seagate HDD',
 										title_ru: '1TB Seagate ЖД', 
@@ -35,7 +25,6 @@ hdd = Product.create(title_en: '1TB Seagate HDD',
 										description_en: Faker::Lorem.paragraph,
 										description_ru: Faker::Lorem.paragraph, 
 										category: hdd_category)
-# copy_image_fixture(fixtures_path.join('hdd.jpg'), hdd.id)
 
 mb = Product.create(title_en: 'Asus P5Q3', 
 										title_ru: 'Asus P5Q5 МП',
@@ -43,9 +32,18 @@ mb = Product.create(title_en: 'Asus P5Q3',
 										description_en: Faker::Lorem.paragraph, 
 										description_ru: Faker::Lorem.paragraph,
 										category: motherboards_category)
-# copy_image_fixture(fixtures_path.join('mb.jpg'), mb.id)
 
-50.times do
+5.times do
+  Product.create(title_en: Faker::Commerce.product_name,
+  							 title_ru: Faker::Commerce.product_name,
+                 price: Faker::Commerce.price,
+                 description_en: Faker::Lorem.paragraph,
+                 description_ru: Faker::Lorem.paragraph,
+                 category: fixtures_category,
+                 image: File.new("#{Rails.root}/public/uploads/images/fixture.jpeg"))
+end
+
+45.times do
   Product.create(title_en: Faker::Commerce.product_name,
   							 title_ru: Faker::Commerce.product_name,
                  price: Faker::Commerce.price,
@@ -54,5 +52,13 @@ mb = Product.create(title_en: 'Asus P5Q3',
                  category: fixtures_category)
 end
 
-user = User.create(name: 'John Doe', email: 'johndoe@example.com', password: '12345678', password_confirmation: '12345678')
-admin_user = User.create(name: 'Admin', email: 'admin@example.com', password: 'adminadmin', password_confirmation: 'adminadmin', admin: true)
+user = User.create(name: 'John Doe', 
+									email: 'johndoe@example.com', 
+									password: '12345678', 
+									password_confirmation: '12345678')
+
+admin_user = User.create(name: 'Admin', 
+												email: 'admin@example.com', 
+												password: 'adminadmin', 
+												password_confirmation: 'adminadmin', 
+												admin: true)
